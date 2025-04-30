01db88632607fa37850a1e0ea6d97cd2a7415587
chimexXy <moadzahnoun37@gmail.com>
2025-04-30 13:35:54 +0100

    chimex

--- philo/philo.h
+++ philo/philo.h
@@ -1,42 +1,45 @@
 #ifndef PHILO_H
-#define PHILO_H
+# define PHILO_H
 
-#include <unistd.h>
-#include <pthread.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <sys/time.h>
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
 
-typedef		struct s_philo{
+typedef struct s_data t_data;
+
+typedef struct s_philo
+{
 	int				id;
-	int				meals;
-	long			last_meal;
-	pthread_t		thread;
-	pthread_mutex_t	*left_fork;
-	pthread_mutex_t	*right_fork;
+	int				left_fork;
+	int				right_fork;
+	int				eat_count;
+	int				last_meal_count;
 	t_data			*data;
-	
-} t_philo;
+	pthread_t		thread;
+}	t_philo;
 
-typedef 	struct s_data{
+typedef struct s_data
+{
 	int				num_philo;
 	int				time_die;
 	int				time_eat;
 	int				time_sleep;
 	int				opt_arg;
-	int				stop;
-	long			start_time;
 
 	pthread_mutex_t	*forks;
-	pthread_mutex_t	print_mutex;
-	pthread_mutex_t	meal_mutex;
-	pthread_mutex_t	stop_mutex;
+	pthread_mutex_t	print;
 
 	t_philo			*philos;
 }	t_data;
 
 // philo
 void	select_things(t_data *philo, char **av);
+void	init_all(t_data *data);
+void	create_threads(t_data *data);
+void    join_threads(t_data *data);
+void    clean_all(t_data *data);
 
 // parsing
 int ft_space_num(char c);
@@ -44,6 +47,7 @@ int check_int(char *str);
 int av_check(int ac, char **av);
 int check_argument(int ac, char **av);
 
+// utils
 int ft_atoi(char *str);
 
 #endif
